8a13bde4efd4b5143b16e6f8789365af8bf39199
johntvale <johntvale@gmail.com>
2021-07-28 15:25:40 -0400

    Requisitos 1, 2, 3 e 4 resolvidos. Aguardando Correcao do linter para fazer o Push

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,48 @@
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText } = this.props;
+    const { bookmarkedOnly } = this.props;
+    const { onSearchTextChange, onBookmarkedChange } = this.props;
     return (
-      <div> </div>
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="text-input">
+            Inclui o texto
+            <input
+              id="text-input"
+              name="text-input"
+              data-testid="text-input"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+            />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+            Mostrar somente favoritos
+            <input
+              id="checkbox-input"
+              data-testid="checkbox-input"
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+            />
+          </label>
+        </form>
+      </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+};
 
 export default SearchBar;
