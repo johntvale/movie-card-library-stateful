54109429deb245731a91fd285026a666a70ccce1
johntvale <johntvale@gmail.com>
2021-07-29 22:42:43 -0400

    Requisito 15 resolvido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+// import SearchBar from './components/SearchBar';
+// import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary />
     </div>
   );
 }
