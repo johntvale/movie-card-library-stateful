ce19e36e8f40712ea87c60eaf44d0ce39fafdf34
johntvale <johntvale@gmail.com>
2022-08-03 14:37:17 -0400

    Updates

---
+++ precache-manifest.aa91198b5dc3f70df42d64933a3d8a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0002338bb302731e059c2ee9b631737",
+    "url": "/movie-card-library-stateful/index.html"
+  },
+  {
+    "revision": "21f68d31b08ce9b26308",
+    "url": "/movie-card-library-stateful/static/css/main.5c7984af.chunk.css"
+  },
+  {
+    "revision": "9bb8dc7015fe34e12f41",
+    "url": "/movie-card-library-stateful/static/js/2.d042edd0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie-card-library-stateful/static/js/2.d042edd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f68d31b08ce9b26308",
+    "url": "/movie-card-library-stateful/static/js/main.98007275.chunk.js"
+  },
+  {
+    "revision": "987679a83aca729e89da",
+    "url": "/movie-card-library-stateful/static/js/runtime-main.91506391.js"
+  }
+]);
